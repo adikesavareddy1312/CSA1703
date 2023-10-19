b660f953de53623a88b8e310d11aa8ee5b63b63c
Adikesavareddy <112241348+adikesavareddy1312@users.noreply.github.com>
2023-10-19 09:12:30 +0530

    Add files via upload

---
+++ exe19-stu_teacher.pl
@@ -0,0 +1,11 @@
+student(praneeth).
+student(chandu).
+student(hemath).
+
+teacher(dr_ajay).
+teacher(ms_akhila).
+teacher(dr_deva).
+
+subject_code(praneeth, csa1234).
+subject_code(chandu, uba1011).
+subject_code(hemanth, dsa1704).
