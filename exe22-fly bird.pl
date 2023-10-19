07949a37f4fa25e0b27642b78df281fab76f3867
Adikesavareddy <112241348+adikesavareddy1312@users.noreply.github.com>
2023-10-19 09:28:28 +0530

    Delete exe22-fly bird.pl

--- exe22-fly bird.pl
+++
@@ -1,9 +0,0 @@
-can_fly(pigeon).
-can_fly(sparrow).
-can_fly(eagle).
-cannot_fly(penguin).
-cannot_fly(ostrich).
-
-
-bird_can_fly(Bird) :- can_fly(Bird).
-bird_can_fly(Bird) :- \+ cannot_fly(Bird).
