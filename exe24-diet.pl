5373bdbdfa2127f131862cbae3bb5dbbd1f42dbe
Adikesavareddy <112241348+adikesavareddy1312@users.noreply.github.com>
2023-10-20 08:17:30 +0530

    Add files via upload

---
+++ exe24-diet.pl
@@ -0,0 +1,4 @@
+diet_suggestion(diabetes, 'Diabetes Diet Plan: Limit carbohydrates, choose whole grains, avoid sugary foods, eat plenty of vegetables and lean proteins.').
+diet_suggestion(hypertension, 'Hypertension Diet Plan: Reduce salt intake, choose low-sodium foods, eat plenty of fruits, vegetables, whole grains, and lean proteins. Avoid processed foods.').
+diet_suggestion(cholesterol,'Cholesterol Diet Plan: Whole grain,Beans,vegetables,fish,sea-food').
+diet_suggestion(IBS,'IBS Diet Plan: White bread,pasta,Whole grains,White rice').
